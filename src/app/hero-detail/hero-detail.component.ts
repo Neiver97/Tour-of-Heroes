6dffd43b992a03a7e9b6a5b6edf53cd1d905aa26
Neiver97 <neiver.tapia@udea.edu.co>
2021-03-22 18:05:15 -0500

    Add navigation

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
+
+
 import { Hero } from '../heroe';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,11 +13,28 @@ import { Hero } from '../heroe';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero?: Hero;
+  hero: Hero | undefined;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
 
   ngOnInit(): void {
+    this.getHero();
+  }
+  
+  getHero(): void {
+    const id =+this.route.snapshot.paramMap.get('id');
+    
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+  this.location.back();
   }
 
 }
