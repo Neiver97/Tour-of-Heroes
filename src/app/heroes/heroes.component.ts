3deb6a7e04b34c963fa60edaf2052cb067bdbbaa
Neiver97 <neiver.tapia@udea.edu.co>
2021-03-22 14:21:05 -0500

    Display a List

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../heroe";
-
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  
   
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  heroes= HEROES;
+
+  selectedHero?: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+
 
 }
